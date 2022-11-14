1f3d960f26a8d80ae361b28770db9d19e1a989eb
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-11-14 13:48:04 +0530

    learn flexbox in css with javascript

---
+++ CssFlexbox/flexbox.js
@@ -0,0 +1,20 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+  //   console.log("hello");
+  this.classList.toggle("open");
+}
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if (e.propertyName.includes("flex")) {
+    this.classList.toggle("open-active", toggleActive);
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
+
+// panels.forEach((panel) => panel.addEventListener("click", toggleActive));
+
+panels.forEach((panel) =>
+  panel.addEventListener("transitionend", toggleActive)
+);
