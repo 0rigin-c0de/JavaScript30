d43c69b8fc1219251ec1a38af7d26604ccff2097
Shunnu <106311923+0rigin-c0de@users.noreply.github.com>
2022-11-22 11:29:06 +0530

    initial commit

--- SpeechSynthesis/index.js
+++
@@ -1,9 +0,0 @@
-const msg = new SpeechSynthesisUtterance();
-//  utterance function going to contain information about how fast say it & what is the pitch & how fast they say it
-let voices = [];
-const voicesDropdown = document.querySelector('[name="voice"]');
-const options = document.querySelectorAll('[type="range"], [name="text"]');
-const speakButton = document.querySelector("#speak");
-const stopButton = document.querySelector("#stop");
-
-msg.text = document.querySelector('[name="text"]').value;
